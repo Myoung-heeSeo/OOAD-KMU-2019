bd4f7ab732bf339af3d45229e02a29e9330d1d97
Myounghee96 <tjaudgml59@gmail.com>
2019-11-06 14:59:29 +0900

    deleted:    Online Musical Reservation System/Admin.txt 	deleted:    Online Musical Reservation System/AdminMode.py 	deleted:    Online Musical Reservation System/Admin_MusicalObj.py 	deleted:    Online Musical Reservation System/BookTicket.py 	deleted:    Online Musical Reservation System/CancelTicket.py 	deleted:    Online Musical Reservation System/CardType.py 	deleted:    Online Musical Reservation System/ChangeSeat.py 	deleted:    Online Musical Reservation System/CheckMyTicket.py 	deleted:    Online Musical Reservation System/CreditCard.py 	deleted:    Online Musical Reservation System/ManageMusicalObject.py 	deleted:    Online Musical Reservation System/Musical.py 	deleted:    Online Musical Reservation System/Musical.txt 	deleted:    Online Musical Reservation System/Order.txt 	deleted:    Online Musical Reservation System/OrderInfo.py 	deleted:    Online Musical Reservation System/PC.py 	deleted:    Online Musical Reservation System/Payment.py 	deleted:    Online Musical Reservation System/Person.txt 	deleted:    Online Musical Reservation System/PersonalInfo.py 	deleted:    Online Musical Reservation System/Seat.py 	deleted:    Online Musical Reservation System/Seat.txt 	deleted:    Online Musical Reservation System/Website.py 	deleted:    Online Musical Reservation System/tempSeat.txt

--- Online Musical Reservation System/CardType.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-class CardType:
-    VISA = 0
-    MASTERCARD = 1
-    AMEX = 2
-    JCB = 3
--- Online Musical Reservation System/PC.py
+++
@@ -1,5 +0,0 @@
-import Website
-
-if __name__ == '__main__':
-    web=Website.Website()
-    web.start()
